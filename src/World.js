23d6f833a5d082840327793e4332799888eafdcd
Tomm Huth <tomm.huth@makingwaves.com>
2018-12-04 13:57:22 +0100

    better fill

--- src/World.js
+++ src/World.js
@@ -3,16 +3,19 @@ import { MeshBuilder } from "babylonjs"
 import uuid from "uuid"
 
 export default class World {
+    width = 40
+    height = 60 
+
     constructor(scene) {
-        const mesh = MeshBuilder.CreateGround(uuid.v4(), { width: 40, height: 40 }, scene)
+        const mesh = MeshBuilder.CreateGround(uuid.v4(), { width: this.width, height: this.height }, scene)
         
-        mesh.position.set(0, -6, 0) 
+        mesh.position.set(0, -6, 20) 
         mesh.visibility = .25
 
         this.group = mesh
     }
  
     beforeRender(pathway, player) {   
-        this.group.position.z = player.position.z
+        this.group.position.z = player.position.z + this.height/2
     }
 }  
