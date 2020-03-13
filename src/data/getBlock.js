b957815d13c8faffa884a9b5a6d271af74565c8e
Tomm Huth <tomm.huth@makingwaves.com>
2020-03-13 03:07:58 +0100

    force coins if too long since last

--- src/data/getBlock.js
+++ src/data/getBlock.js
@@ -3,19 +3,27 @@ import BlockType from "./const/BlockType"
 import BlockTypeSetting from "./const/BlockTypeSetting"
 import random from "./random"
 
+let countSinceCoins = 0
+
 export default function getBlock(previous) {
-    let blocks = [...Object.values(BlockType), BlockType.OBSTACLES]
+    let blocks = countSinceCoins > 3 ? [BlockType.PLAIN] : [...Object.values(BlockType)]
     let options = {
         type: random.pick(blocks),
         id: uuid.v4(),
         start: previous.end
     } 
 
-    while (BlockTypeSetting[previous.type].illegalNext.includes(options.type)) { 
+    while (!random.bool(BlockTypeSetting[options.type].likelyhood) || BlockTypeSetting[previous.type].illegalNext.includes(options.type)) { 
         options.type = random.pick(blocks)
     }
 
     options.depth = random.integer(...BlockTypeSetting[options.type].depth)
+
+    if (options.type !== BlockType.PLAIN) {
+        countSinceCoins++
+    } else {
+        countSinceCoins = 0
+    } 
 
     return {
         ...options,
