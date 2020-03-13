168993ecf7634395f196cb89abab184b457cb75d
Tomm Huth <tomm.huth@makingwaves.com>
2020-03-13 03:08:11 +0100

    random x

--- src/components/blocks/PlainBlock.js
+++ src/components/blocks/PlainBlock.js
@@ -1,18 +1,22 @@
-import React from "react" 
+import React, { useState } from "react"
 import CoinLine from "../CoinLine"
+import random from "../../data/random"
 
 export default function PlainBlock({
     depth,
-    start, 
+    start,
     y
-}) { 
+}) {
+    let [coinX] = useState(() => random.integer(-4, 4))
+
     return (
         <>
             <CoinLine
                 depth={depth}
+                x={coinX}
                 z={start + depth / 2}
                 y={y}
-            /> 
+            />
         </>
     )
 }
