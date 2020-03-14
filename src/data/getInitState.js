6d65bbc5e2788437dbebc4923d20efc0e0479404
Tomm Huth <tomm.huth@makingwaves.com>
2020-03-14 21:18:36 +0100

    less starttime

--- src/data/getInitState.js
+++ src/data/getInitState.js
@@ -38,7 +38,7 @@ export default function getInitState() {
         state: mustRequestOrientationAccess ? GameState.REQUEST_ORIENTATION_ACCESS : GameState.READY,
         score: 0,
         attempts: 0,
-        time: 1000 * 30,
+        time: 1000 * 20,
         reason: null,
         position: { x: 0, y: 0, z: 30 }
     }
