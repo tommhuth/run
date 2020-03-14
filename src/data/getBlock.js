4b3cf5f32cdf52a3e24e9af6c9f9bdde0f051c57
Tomm Huth <tomm.huth@makingwaves.com>
2020-03-14 01:09:34 +0100

    ignore loop check if forced type

--- src/data/getBlock.js
+++ src/data/getBlock.js
@@ -6,24 +6,25 @@ import random from "./random"
 let countSinceCoins = 0
 
 export default function getBlock(previous) {
-    let blocks = countSinceCoins > 3 ? [BlockType.PLAIN] : [...Object.values(BlockType)]
+    let forceCoins = countSinceCoins > 3
+    let blocks = forceCoins ? [BlockType.PLAIN] : [...Object.values(BlockType)]
     let options = {
         type: random.pick(blocks),
         id: uuid.v4(),
         start: previous.end
-    } 
+    }
 
-    while (!random.bool(BlockTypeSetting[options.type].likelyhood) || BlockTypeSetting[previous.type].illegalNext.includes(options.type)) { 
+    while (!forceCoins && (!random.bool(BlockTypeSetting[options.type].likelyhood) || BlockTypeSetting[previous.type].illegalNext.includes(options.type))) {
         options.type = random.pick(blocks)
     }
 
     options.depth = random.integer(...BlockTypeSetting[options.type].depth)
 
-    if (options.type !== BlockType.PLAIN) {
-        countSinceCoins++
-    } else {
+    if (options.type === BlockType.PLAIN) {
         countSinceCoins = 0
-    } 
+    } else {
+        countSinceCoins++
+    }
 
     return {
         ...options,
