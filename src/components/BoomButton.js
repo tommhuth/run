2b9b2bf3250e76a12e87fb0d88be1b0f4a55eaa3
Tomm Huth <tomm.huth@makingwaves.com>
2020-03-19 03:00:35 +0100

    better active styling

--- src/components/BoomButton.js
+++ src/components/BoomButton.js
@@ -1,11 +1,20 @@
-import React from "react" 
+import React, { useState } from "react"
+import cn from "classnames"
+
 import "./style/boom-button.scss"
 
 export default function BoomButton({ boom }) {
+    let [active, setActive] = useState(false)
+
     return (
         <button
-            className="boom-button"
+            className={cn("boom-button", { "boom-button--active": active })}
             type="button"
+            onMouseOver={() => setActive(true)}
+            onMouseOut={() => setActive(false)}
+            onTouchStart={() => setActive(true)}
+            onTouchEnd={() => setActive(false)}
+            onTouchCancel={() => setActive(false)}
             onClick={(e) => {
                 e.stopPropagation()
                 boom()
