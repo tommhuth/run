d91583972bb1d111833892c417a11a3b26db7fbc
Tomm Huth <>
2020-08-22 11:11:54 +0200

    text tweaks

--- src/ui/FontLoader.js
+++ src/ui/FontLoader.js
@@ -4,11 +4,14 @@ import FontFaceObserver from "fontfaceobserver"
 export default function FontLoader({
     children
 }) {
-    let fonts = useMemo(() => new FontFaceObserver("Oswald", {  weight: 300, style: "normal" }), [])
+    let fonts = useMemo(() =>  [
+        new FontFaceObserver("Oswald", {  weight: 200, style: "normal" }).load(),
+        new FontFaceObserver("Oswald", {  weight: 500, style: "normal" }).load()
+    ], [])
     let [loading, setLoading] = useState(true)
     let load = useCallback(async () => {
         try {
-            await fonts.load()
+            await Promise.all(fonts)
         } catch (e) {
             // do nothing
         } finally {
