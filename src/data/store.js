6ddcd6e10107a795135a9f5e85f18d5fffd75047
Tomm Huth <>
2020-08-26 19:14:54 +0200

    refactor

--- src/data/store.js
+++ src/data/store.js
@@ -4,6 +4,7 @@ import Config from "../Config"
 import random from "@huth/random"
 import GameState from "../data/const/GameState"
 import BlockType from "./const/BlockType"
+import { getBlock } from "./utils"
 
 const mustRequestOrientationAccess = window.DeviceOrientationEvent && window.DeviceOrientationEvent.requestPermission
 const initState = {
@@ -218,87 +219,6 @@ const [useStore, api] = create((set, get) => {
     }
 })
 
-let i = 0
-
-function getNextStep(blocks) {
-    let next = i % 3 === 0 && i > 0 ? blocks[0].step : random.pick(-2, 2)
-
-    return next
-}
-
-function getBlock(blocks) {
-    let previous = blocks[0]
-    let stepUp = [BlockType.NARROW].includes(previous.type) ? false : random.boolean(.9)
-    let type = getNextType(previous)
-    let block = {
-        id: uuid.v4(),
-        start: previous.end,
-        width: random.integer(Config.BLOCK_WIDTH, Config.BLOCK_WIDTH + Config.BLOCK_MAX_EXTRA_WIDTH),
-        previousType: previous.type,
-        stepUp,
-        type,
-        ...makeBlock[type](previous)
-    }
-
-    let step = getNextStep(blocks)
-
-    i++
-
-    return {
-        ...block,
-        end: block.start + block.depth,
-        y: previous.y + (block.stepUp ? step : 0),
-        step: block.stepUp ? step : 0
-    }
-}
-
-function getNextType(previous) {
-    let types = [
-        ...Object.values(BlockType).filter(i => i !== BlockType.START),
-        BlockType.OBSTACLES,
-        BlockType.OBSTACLES,
-        BlockType.OBSTACLES,
-        BlockType.OBSTACLES,
-    ]
-    let illegalNext = {
-        [BlockType.START]: [BlockType.NARROW],
-        [BlockType.PLAIN]: [BlockType.PLAIN],
-        [BlockType.OBSTACLES]: [],
-        [BlockType.NARROW]: []
-    }
-    let type = random.pick(...types)
-
-    while (illegalNext[previous.type].includes(type)) {
-        type = random.pick(...types)
-    }
-
-    return type
-}
-
-const makeBlock = {
-    [BlockType.START]() {
-        return {
-            depth: 35
-        }
-    },
-    [BlockType.PLAIN]() {
-        return {
-            depth: random.integer(7, 10)
-        }
-    },
-    [BlockType.OBSTACLES]() {
-        return {
-            depth: random.integer(15, 20)
-        }
-    },
-    [BlockType.NARROW](previous) {
-        return {
-            depth: previous.type === BlockType.NARROW ? 11 : 16,
-            width: 7,
-            stepUp: false
-        }
-    }
-}
 
 
 export { api, useStore }
---
+++ src/data/utils.js
@@ -0,0 +1,86 @@
+import random from "@huth/random"
+import Config from "../Config"
+import uuid from "uuid"
+import BlockType from "./const/BlockType"
+
+let i = 0
+
+function getNextStep(blocks) {
+    let next = i % 3 === 0 && i > 0 ? blocks[0].step : random.pick(-2, 2)
+
+    return next
+}
+
+function getNextType(previous) {
+    let types = [
+        ...Object.values(BlockType).filter(i => i !== BlockType.START),
+        BlockType.OBSTACLES,
+        BlockType.OBSTACLES,
+        BlockType.OBSTACLES,
+        BlockType.OBSTACLES,
+    ]
+    let illegalNext = {
+        [BlockType.START]: [BlockType.NARROW],
+        [BlockType.PLAIN]: [BlockType.PLAIN],
+        [BlockType.OBSTACLES]: [],
+        [BlockType.NARROW]: []
+    }
+    let type = random.pick(...types)
+
+    while (illegalNext[previous.type].includes(type)) {
+        type = random.pick(...types)
+    }
+
+    return type
+}
+
+const makeBlock = {
+    [BlockType.START]() {
+        return {
+            depth: 35
+        }
+    },
+    [BlockType.PLAIN]() {
+        return {
+            depth: random.integer(7, 10)
+        }
+    },
+    [BlockType.OBSTACLES]() {
+        return {
+            depth: random.integer(15, 20)
+        }
+    },
+    [BlockType.NARROW](previous) {
+        return {
+            depth: previous.type === BlockType.NARROW ? 11 : 16,
+            width: 7,
+            stepUp: false
+        }
+    }
+} 
+
+export function getBlock(blocks) {
+    let previous = blocks[0]
+    let stepUp = [BlockType.NARROW].includes(previous.type) ? false : random.boolean(.9)
+    let type = getNextType(previous)
+    let block = {
+        id: uuid.v4(),
+        start: previous.end,
+        width: random.integer(Config.BLOCK_WIDTH, Config.BLOCK_WIDTH + Config.BLOCK_MAX_EXTRA_WIDTH),
+        previousType: previous.type,
+        stepUp,
+        type,
+        ...makeBlock[type](previous)
+    }
+
+    let step = getNextStep(blocks)
+
+    i++
+
+    return {
+        ...block,
+        end: block.start + block.depth,
+        y: previous.y + (block.stepUp ? step : 0),
+        step: block.stepUp ? step : 0
+    }
+}
