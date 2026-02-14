eb5e467f73ea62b403766510a05482fd1eb9a5d2
Tomm Huth <tomm.huth@hotmail.com>
2026-02-14 14:18:38 +0100

    back, not a fix

--- src/data/cannon.tsx
+++ src/data/cannon.tsx
@@ -175,7 +175,7 @@ export function CannonProvider({
         // max 30fps as delta
         const dt = Math.min(delta, 1 / 30)
 
-        world.step(dt)
+        world.fixedStep(dt)
 
         if (world.hasActiveBodies && cannonDebugger) {
             cannonDebugger.update()
