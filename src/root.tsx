9f1ee032c0a6e087569897b5d7f307cc17f2b57d
Tomm Huth <tomm.huth@hotmail.com>
2026-02-19 22:52:31 +0100

    gone

--- src/root.tsx
+++ src/root.tsx
@@ -3,16 +3,9 @@ import { clamp } from "@data/utils"
 import { createRoot, events, RenderProps } from "@react-three/fiber"
 import { lazy, ReactNode } from "react"
 import { createRoot as createUiRoot } from "react-dom/client"
-import { Viewportify } from "viewportify"
 import { registerSW } from "virtual:pwa-register"
 
 import Ui from "./ui/Ui"
-
-new Viewportify({
-    setCSSVariables: true,
-    iosVhFix: true
-})
-
 
 function getConfiguration() {
     return {
