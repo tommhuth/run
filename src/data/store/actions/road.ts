627d316bd3ffff3187f7622aaa17a627aae73b9e
Tomm Huth <tomm.huth@hotmail.com>
2026-05-26 22:20:28 +0200

    further off

--- src/data/store/actions/road.ts
+++ src/data/store/actions/road.ts
@@ -53,11 +53,9 @@ export function generateBridgePart(previous: PreviousPart): RoadPart {
     }
 }
 
-
-
 export function reachDestination() {
     const player = store.getState().player
-    const nextTargetAt = player.nextTargetAt + random.integer(ROAD_FORWARD_EDGE * 1.5, ROAD_FORWARD_EDGE * 4)
+    const nextTargetAt = player.nextTargetAt + random.integer(ROAD_FORWARD_EDGE * 1.5, ROAD_FORWARD_EDGE * 6)
     const targetDistance = nextTargetAt - player.nextTargetAt
     const secondsPerMeter = .078
     const deadlineInSeconds = targetDistance * secondsPerMeter
